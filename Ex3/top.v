7040801be7f3054a28b021def4bd6c1da943a165
Cloud User <centos@ip-172-31-12-128.eu-west-2.compute.internal>
2020-06-01 15:35:12 +0000

    My solution to Ex3

--- Ex3/top.v
+++ Ex3/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #3 
-// Student Name:
-// Date: 
+// Student Name: Pawit Kochakarn
+// Date: 01/06/2020
 //
 //  Description: In this exercise, you need to design an up / down counter, where 
 //  if the rst=1, the counter should be set to zero. If enable=0, the value
@@ -20,11 +20,23 @@
 
 module counter(
     //Todo: add ports 
-
-    );
+	clk,rst,enable,direction,counter_out
+	);
                     
     //Todo: add registers and wires, if needed
+	input clk, rst, enable, direction;
+	output reg[7:0]counter_out;
+
 
     //Todo: add user logic
-      
+	always @(posedge clk or posedge rst) begin
+		if (rst) 
+		counter_out = 8'd0;
+		if (enable) begin
+			if (direction)
+			counter_out <= counter_out + 1;
+			else 
+			counter_out <= counter_out - 1;
+		end	
+      	end
 endmodule
--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #3 - Up/Down counter
-// Student Name:
-// Date: 
+// Student Name: Pawit Kochakarn
+// Date: 01/06/2020
 //
 // Description: A testbench module to test Ex3 - counter
 // Guidance: start with simple tests of the module (how should it react to each 
@@ -10,19 +10,42 @@
 //////////////////////////////////////////////////////////////////////////////////
 `timescale 1ns / 100ps
 
-module top_tb(
-    );
+module top_tb();
     
 //Todo: Parameters
+parameter CLK_PERIOD = 10;
 
-//Todo: Regitsers and wires
+//Todo: Registers and wires
+reg clk,rst,enable,direction;
+wire [7:0] counter_out;
+
 
 //Todo: Clock generation
+initial begin
+clk= 1'b0;
+forever 
+#(CLK_PERIOD/2) clk=~clk;
+end
 
 //Todo: User logic
+initial begin
+rst=1;
+enable=0;
+direction=1;
+#10
+enable=1;
+direction=1;
+#20
+direction=0;
+#10
+direction=1;
+$display("counter_out=%h",counter_out);
+end
     
-//Todo: Finish test, check for success
+//Todo: Finish test, check for successclear
 
 //Todo: Instantiate counter module
+counter my_counter(clk,rst,enable,direction,counter_out);
+
  
 endmodule 
