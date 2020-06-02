c41aa4e83c5af8727a68e405c7690151b6bf1ec8
Cloud User <centos@ip-172-31-20-164.ap-southeast-1.compute.internal>
2020-06-02 09:36:41 +0000

    My solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name: Pawit Kochakarn	
+// Date: 02/06/2020
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -10,6 +10,37 @@
 //           clk
 //
 //  outputs:
-//           red, amber, green
+//           red , amber, green
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module traffic_lights(clk,red,amber,green);
+
+	input clk;
+	output reg red,amber,green;
+
+	reg [2:0] state;
+
+	always @(posedge clk) begin
+		case(state)
+		3'b001 : state = 3'b011; //red --> red+amber
+		3'b011 : state = 3'b100; //red+amber --> green
+		3'b100 : state = 3'b010; //green --> amber
+		3'b010 : state = 3'b001; //amber --> red
+		default : state = 3'b001; //start at red
+		endcase
+	end
+
+	always @(state) begin
+		case(state)
+		3'b001 : {green,amber,red} = 3'b001; //red
+		3'b011 : {green,amber,red} = 3'b011; //red+amber
+		3'b100 : {green,amber,red} = 3'b100; //green
+		3'b010 : {green,amber,red} = 3'b010; //amber
+		endcase
+	end
+
+
+endmodule
 
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,30 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Traffic Lights
-// Student Name:
-// Date: 
+// Student Name: Pawit Kochakarn
+// Date: 02/06/2020
 //
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clk;
+wire red, amber, green;
+
+initial begin
+clk = 1'b0;
+forever 
+#(CLK_PERIOD/2) clk=~clk;
+$display("red=%h",red);
+$display("amber=%h",amber);
+$display("green=%h",green);
+end
+
+traffic_lights mytraffic(clk,red,amber,green);
+
+endmodule
