58f17ac21cc55457e255d81fd20398093790f017
Cloud User <centos@ip-172-31-20-164.ap-southeast-1.compute.internal>
2020-06-03 10:09:55 +0000

    Updated Ex6 Solution

--- Ex6/top.v
+++ Ex6/top.v
@@ -20,3 +20,19 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+module top(
+	input clk,rst,button,sel,
+	output [2:0]result
+);
+
+wire [2:0]tmp_1, r, a, g;
+
+
+dice my_dice(.clk(clk),.rst(rst),.button(button),.throw(tmp_1));
+traffic_lights trafic_lights(.clk(clk),.red(r),.amber(a),.green(g));
+mux my_mux(.a(tmp_1),.b({g,a,r}),.sel(sel),out(result));
+
+
+endmodule
+
+
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -6,4 +6,31 @@
 // Description: A testbench module to test Ex6 - Dice or Traffic Lights?
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clk, rst, button, sel;
+wire result;
+
+initial begin
+clk = 1'b0;
+forever 
+#(CLK_PERIOD/2) clk=~clk;
+end
+
+initial begin
+rst=1;
+#10
+rst=0;
+button=1;
+#200
+button=0;
+sel=0;
+$display("output=%h",result);
+end
+
+endmodule
 
