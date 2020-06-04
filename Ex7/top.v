04e40d05d7a9741f2ca2867f9878fcf66668d815
Cloud User <centos@ip-172-31-20-164.ap-southeast-1.compute.internal>
2020-06-04 12:10:16 +0000

    Ex7 so far

--- Ex7/top.v
+++ Ex7/top.v
@@ -3,14 +3,25 @@
 // Student Name:
 // Date: 
 //
-//  Description: In this exercise, you need to implement a times table of 0..7x0..7
-//  using a memory.
+// Description: In this exercise, you need to implement a times table of 0..7x0..7
+// using a memory.
+// 
+// inputs:
+// 		clk, a[2:0], b[2:0], read
 //
-//  inputs:
-//           clk, a[2:0], b[2:0], read
-//
-//  outputs:
-//           result[4:0]
+// output:
+//		result[4:0]
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
 
+module times(input clk, [2:0]a, [2:0]b, read,
+		output [5:0]result);
+
+	wire [5:0] ab;
+	assign ab = {a,b}; //concat ab in order to look up addr in mem
+	
+	my_bram bram_1(.clka(clk),.ena(read),.wea(1'b0),.addra(ab),.dina(6'd0),.douta(result));
+
+
+endmodule
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -1,9 +1,54 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #7 - Times Table
-// Student Name:
-// Date: 
+// Student Name: Pawit Kochakarn
+// Date: 04/06/2020
 //
 // Description: A testbench module to test Ex7 - Times Table
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clk, err, [2:0]a, [2:0]b, read;
+wire [5:0] result;
+
+initial begin
+read=1;
+#300
+read=0;
+
+end
+
+initial begin
+clk = 1'b0;
+forever 
+#(CLK_PERIOD/2) clk=~clk;
+
+end
+
+initial begin
+a = 0;
+b = 0;
+err = 0;
+
+forever begin
+	#CLK_PERIOD
+	$display("Result=%h",result);
+	if(read) && a*b!=result)
+		begin
+		$display("Error, miscalc");
+		err = 1;
+		end
+	a = a+1; //go through whole ...7x...7 timestables
+	if (a==0)
+		b = b+1;
+	end
+end
+
+times bram( .clk(clk), .a(a), .b(b), .result(result)); //instance
+
+
+endmodule
