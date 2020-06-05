50ebaa71711dd7975acd5e45d426066860ea7b2b
Cloud User <centos@ip-172-31-20-164.ap-southeast-1.compute.internal>
2020-06-05 12:18:27 +0000

    My solution to Ex9

--- Ex9/top.v
+++ Ex9/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #9 
-// Student Name:
-// Date: 
+// Student Name: Pawit Kochakarn
+// Date: 05/06/2020
 //
 //  Description: In this exercise, you need to design a module that lights leds
 //  at different frequencies
@@ -17,12 +17,15 @@
 module top(
     input clk_p,
     input clk_n,
+    input rst_n, //active LOW
+    input button,
+    output reg led_0,led_1,led_2
      //Todo: add all other ports besides clk_n and clk_p 
    );
     
 
    /* clock infrastructure, do not modify */
-        wire clk_ibufds;
+    wire clk_ibufds;
 
     IBUFDS IBUFDS_sysclk (
 	.I(clk_p),
@@ -36,7 +39,25 @@ module top(
 	.I  (clk_ibufds),
 	.O  (clk)
       );
-
 //Add logic here
+
+	reg [20:0]counter = 21'd0; //21-bit up counter to 2e6 for 50Hz LED blink cycle (on+off)
+
+	always @(posedge clk, posedge rst_n) begin
+		if (~rst_n && button) begin //active low reset + button
+			{led_2,led_1,led_0} = 3'b0;
+			counter = 0;
+		end
+		else if (counter==21'd1000000) begin //LED ON
+			{led_2,led_1,led_0} = 3'b111;
+		end
+		else if (counter==21'd2000000) begin //LED OFF
+			{led_2,led_1,led_0} = 3'b0;
+			counter = 0; 
+		end
+		else begin
+			counter = counter + 1;
+		end
+	end
 
 endmodule
